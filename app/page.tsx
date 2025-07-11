8dacdd69183108359952ec27cda250ad663af985
nitishml <nitishml25@gmail.com>
2025-07-11 15:54:44 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,8 @@
-import Image from "next/image";
-import './globals.css'
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen text-center space-y-4 bg-[#8FC73E]">
-     
         <h1 className="text-4xl text-white semibold-font text-center">PRASHI HR CONSULTANCY</h1>
         <h1 className="text-2xl text-white bold-font  ">Comming Soon.......</h1>
-      
-     
     </div>
   );
 }
